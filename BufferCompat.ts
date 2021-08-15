149cf58f9e8f4e66a1b3cd868b84ab1cbc400df9
Annabelle Kennedy <queekusme@users.noreply.github.com>
2021-08-15 11:16:43 +0100

    Fix BufferCompat being commented out for test

--- BufferCompat.ts
+++ BufferCompat.ts
@@ -1,16 +1,16 @@
 import { Buffer } from "./deps.ts";
 
-// // deno-lint-ignore no-explicit-any
-// if(!(Buffer.prototype as any).swap16)
-// {
-//     // deno-lint-ignore no-explicit-any
-//     (Buffer.prototype as any).swap16 = function ()
-//     {
-//         for(let i = 0; i < (this as unknown as Buffer).buffer.byteLength; i += 2)
-//         {
-//             const temp = (this as unknown as Buffer)[i];
-//             (this as unknown as Buffer)[i] = (this as unknown as Buffer)[i + 1];
-//             (this as unknown as Buffer)[i + 1] = temp;
-//         }
-//     }
-// }
+// deno-lint-ignore no-explicit-any
+if(!(Buffer.prototype as any).swap16)
+{
+    // deno-lint-ignore no-explicit-any
+    (Buffer.prototype as any).swap16 = function ()
+    {
+        for(let i = 0; i < (this as unknown as Buffer).buffer.byteLength; i += 2)
+        {
+            const temp = (this as unknown as Buffer)[i];
+            (this as unknown as Buffer)[i] = (this as unknown as Buffer)[i + 1];
+            (this as unknown as Buffer)[i + 1] = temp;
+        }
+    }
+}
