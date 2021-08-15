ed8948344963c2fdf813eb58d1e4256433e8526c
Annabelle Kennedy <queekusme@users.noreply.github.com>
2021-08-15 11:13:17 +0100

    updated exports to export default cleaning up mod.ts

--- mod.ts
+++ mod.ts
@@ -2,7 +2,5 @@ export type { Bit, Class, Opcode, QR, QTypeAdditionals, ReturnCode, Type, DNSPro
 export { DNSProtocol, DNSProtocolHeader, DNSProtocolQuestion, DNSProtocolResourceRecord } from "./ProtocolClasses.ts";
 export { IPv4, IPv6 } from "./IPAddress.ts";
 export { UInt4, UInt8, UInt16, UInt32 } from "./UInt.ts";
-import Parser from "./Parser.ts";
-export { Parser };
-import DomainName from "./DomainName.ts";
-export { DomainName };
+export { default as Parser } from "./Parser.ts";
+export { default as DomainName } from "./DomainName.ts";
--- test.ts
+++ test.ts
@@ -2,6 +2,10 @@ import { Buffer } from "./deps.ts";
 import { IPv6 } from "./IPAddress.ts";
 
 const decoded = new IPv6();
-decoded.decode(Buffer.from([32, 1, 72, 96, 72, 96, 0, 0, 0, 0, 0, 0, 0, 0, 136, 136]));
+const data = Buffer.from([32, 1, 72, 96, 72, 96, 0, 0, 0, 0, 0, 0, 0, 0, 136, 136]);
+decoded.decode(data);
+// @ts-ignore foo
+data.swap16();
+console.log(data)
 
 console.log(`decoded.value === "2001:4860:4860:0:0:0:0:8888 ? ${decoded.value === "2001:4860:4860:0:0:0:0:8888"}"`);
